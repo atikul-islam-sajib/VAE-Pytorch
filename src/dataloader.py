1b0635d61e433fd94b34f472524eeba4055e1b04
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-07-09 12:13:33 +0200

    unzip is done

--- src/dataloader.py
+++ src/dataloader.py
@@ -4,6 +4,8 @@ import zipfile
 
 sys.path.append("src/")
 
+from utils import config, CustomException
+
 
 class Loader:
     def __init__(self, image_path=None, channels=3, image_size=256, split_size=0.20):
@@ -13,4 +15,22 @@ class Loader:
         self.split_size = split_size
 
     def unzip_folder(self):
-        pass
+        self.raw_data_path = config()["path"]["RAW_DATA_PATH"]
+
+        if os.path.exists(self.raw_data_path):
+            with zipfile.ZipFile(self.image_path, "r") as zip_file:
+                zip_file.extractall(path=os.path.join(self.raw_data_path))
+
+            print(
+                "Unzip is done successfully and stoed in the path {}".format(
+                    os.path.join(self.raw_data_path, "dataset")
+                )
+            )
+
+        else:
+            raise CustomException("Raw data path does not exist".capitalize())
+
+
+if __name__ == "__main__":
+    loader = Loader(image_path="./data/raw/dataset1.zip")
+    # loader.unzip_folder()
--- src/utils.py
+++ src/utils.py
@@ -4,3 +4,8 @@ import yaml
 def config():
     with open("./config.yml", "r") as file:
         return yaml.safe_load(file)
+
+
+class CustomException(Exception):
+    def __init__(self, message=None):
+        super().__init__(message)
