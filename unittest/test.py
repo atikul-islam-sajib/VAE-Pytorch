a2ad983a802b919a2f67a2675af97cf047311688
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-07-10 13:20:53 +0200

    unittest file is added

---
+++ unittest/test.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import torch
+import unittest
+import torch.nn as nn
+
+sys.path.append("src/")
+
+from dataloader import Loader
+from encoder import EncoderBlock
+from decoder import DecoderBlock
+from VAE import VariationalAutoEncoder
+from helper import helpers
+from utils import load, config
+
+
+class UnitTest(unittest.TestCase):
+    def setUp(self):
+        self.train_dataloader = load(
+            filename=os.path.join(
+                config()["path"]["PROCESSED_DATA_PATH"], "train_dataloader.pkl"
+            )
+        )
+        self.valid_dataloader = load(
+            filename=os.path.join(
+                config()["path"]["PROCESSED_DATA_PATH"], "valid_dataloader.pkl"
+            )
+        )
+
+    def test_dataloader(self):
+        self.assertEqual(
+            self.train_dataloader.__class__, torch.utils.data.dataloader.DataLoader
+        )
+        self.assertEqual(
+            self.valid_dataloader.__class__, torch.utils.data.dataloader.DataLoader
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
