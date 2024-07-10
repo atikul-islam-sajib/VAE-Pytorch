1d69a3f9fa0218dcc9f21e662a38174fab549974
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-07-10 13:25:30 +0200

    Encoder & Decoder passed the unittest

--- unittest/test.py
+++ unittest/test.py
@@ -41,6 +41,51 @@ class UnitTest(unittest.TestCase):
     def test_quantity_valid_dataloader(self):
         self.assertEqual(sum(X.size(0) for X, _ in self.valid_dataloader), 6)
 
+    def test_encoder(self):
+        in_channels = 3
+        out_channels = 128
+
+        layers = []
+
+        for _ in range(2):
+            layers.append(
+                EncoderBlock(in_channels=in_channels, out_channels=out_channels)
+            )
+            in_channels = out_channels
+            out_channels //= 2
+
+        model = nn.Sequential(*layers)
+
+        self.assertEqual(
+            model(torch.randn(1, 3, 256, 256)).size(), torch.Size([1, 64, 64, 64])
+        )
+
+    def test_decoder(self):
+        layers = []
+
+        in_channels = 64
+        out_channels = 128
+
+        layers.append(DecoderBlock(in_channels=in_channels, out_channels=out_channels))
+
+        in_channels = out_channels
+
+        layers.append(
+            nn.ConvTranspose2d(
+                in_channels=in_channels,
+                out_channels=3,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+            )
+        )
+
+        model = nn.Sequential(*layers)
+
+        self.assertEqual(
+            model(torch.randn(1, 64, 64, 64)).size(), torch.Size([1, 3, 256, 256])
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
